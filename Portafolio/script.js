cf3e4523978bd18506d1426ec5d776d79968d089
Antonio Gutierrez <91162821+tonigutierrez@users.noreply.github.com>
2021-12-08 11:52:20 -0300

    Portafolio Final (Curso Desarrollador Frontend)

--- Portafolio/script.js
+++ Portafolio/script.js
@@ -10,8 +10,22 @@ function sendEmail(e){
     const form = new FormData (this);
     $boton.setAttribute('href', `mailto:antoniog.devel@gmail.com?subject=${form.get('name')} ${form.get('email')}&body=${form.get('message')}`);
     $boton.click();
+    alert('Abriendo su software de emails')
 }
-
 
 // Boton ir al cielo //////////////////////////////////
 
+$(document).ready(function() {
+    $('.gotop').click(function() {
+        $('body, html').animate({
+            scrollTop: 0
+        }, 300)
+    });
+    $(window).scroll(function() {
+        if($(this).scrollTop()> 300){
+            $('.gotop').slideDown(300);
+        }else{
+            $('.gotop').slideUp(300);
+        }
+    })
+})
